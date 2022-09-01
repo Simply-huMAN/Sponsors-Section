15da666f84fd0e7679877a8a41db09c58d0e39c3
Simply-huMAN <76241066+Simply-huMAN@users.noreply.github.com>
2022-09-01 22:29:13 +0530

    my first react website commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import Sponsors from "./components/Sponsors";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <div className="main">
+        <div className="content">
+          <p id="heading">Our Awesome Sponsors</p>
+          <p id="info">
+            Our love for open source and drive to make an impact materialized as
+            HackNITR 4.0. Backed by likes of participating students, we grew
+            steadily to organize hackathons and support social intiatives.
+            Today, we bring you the opportunity to collaborate with us on our
+            next endeavour. &nbsp;
+          </p>
+          <a
+            id="link"
+            href="https://form.typeform.com/to/maI5lJ4t?typeform-source=hacknitr21.typeform.com"
+            target="blank"
+          >
+            Sponsor us
+          </a>
+        </div>
+        <Sponsors />
+        {/* <div className="sponsors">
+        <p>ALPHA SPONSORS</p>
+        <a href="https://mlh.io" target="blank">
+          <img
+            src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/MLH_xejp6v.png"
+            alt="MLH logo"
+            // style="width: 80%"
+          ></img>
         </a>
-      </header>
-    </div>
+        <a href="https://github.com" target="blank">
+          <img
+            src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/github1_cujncr.png"
+            alt="Github"
+            // style="width:70%"
+          ></img>
+        </a>
+        <a href="https://devfolio.co/" target="blank">
+          <img
+            src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/past_rv4xvj.png"
+            alt="Devfolio"
+            // style="width:95%"
+          ></img>
+        </a>
+      </div>
+      <div className="sponsors">
+        <p>BETA SPONSORS</p>
+      </div>
+      <div className="sponsors">
+        <p>GAMMA SPONSORS</p>
+      </div>
+      <div className="sponsors">
+        <p>DELTA SPONSORS</p>
+      </div>
+      <div className="partners">
+        <p>MEDIA PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>AUTHENTICATION PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>MENTAL HEALTH PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>CODING PARTNERS</p>
+      </div> */}
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Sponsors.js
@@ -0,0 +1,269 @@
+import "./sponsors.css";
+
+function Sponsors() {
+  return (
+    <div>
+      <div className="sponsors">
+        <p>ALPHA SPONSORS</p>
+        <div className="image">
+          <div className="images">
+            <a href="https://mlh.io" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/MLH_xejp6v.png"
+                alt="MLH logo"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://github.com" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/github1_cujncr.png"
+                alt="Github"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://devfolio.co/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/past_rv4xvj.png"
+                alt="Devfolio"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="sponsors">
+        <p>BETA SPONSORS</p>
+        <div className="image">
+          <div className="images">
+            <a href="https://a0.to/hacknitr" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/auth0_ziq5l3.png"
+                alt="Auth0"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://egghead.io/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/egghead_ie5qbb.png"
+                alt="Egghead"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://codedamn.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/codedamn_prbhnb.png"
+                alt="Codedamn"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://www.sashido.io/en/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/sashido_xiu6is.png"
+                alt="Sashido"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://gen.xyz/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/xyz_ldytud.png"
+                alt=".xyz"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://celo.org/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/celo-removebg-preview_b7usib.png"
+                alt="Celo"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://digitalocean.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/DO_mjgx0y.png"
+                alt="DigitalOcean"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://polygon.technology/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/polygon_vvmhvg.png"
+                alt="Polygon"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://tezos.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/tezos_cnqvdn.png"
+                alt="Tezos"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://filecoin.io/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/Filecoin_White_oztcqi.png"
+                alt="Filecoin"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="sponsors">
+        <p>GAMMA SPONSORS</p>
+        <div className="image">
+          <div className="images">
+            <a href="https://echoar.xyz/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/echoAR_-_Logo_2020_-_Knockout_c9smwf.png"
+                alt="EchoAR"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://linode.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/lincode_y9pcdh.png"
+                alt="Linode"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://stickermule.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/sticker-mule_j0soly.svg"
+                alt="StickerMule"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://wolfram.com/language/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/wolfram_sid6kv.png"
+                alt="Wolfram"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://koii.network/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/koii_wsget6.png"
+                alt="Koii Network"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="sponsors">
+        <p>DELTA SPONSORS</p>
+        <div className="image">
+          <div className="images">
+            <a href="https://replit.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/Untitled_nqdjaf.png"
+                alt="Repl.it"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://taskade.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/taskade-circle-logo-full-white_iakfy1.png"
+                alt="Taskade"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://tweetbeam.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/edited_tweetbeam_x3dv1r.png"
+                alt="Tweetbeam"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://voiceflow.com/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/Voiceflow_White_iczupf.png"
+                alt="Voiceflow"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a
+              href="https://www.instagram.com/filter_beans/?hl=en"
+              target="blank"
+            >
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/filterbeans-removebg-preview_jidk5d.png"
+                alt="Filter_Beans"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://leadlearners.ca/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/leadinglearners_vfs68g.png"
+                alt="Leading Learners"
+              ></img>
+            </a>
+          </div>
+          <div className="images">
+            <a href="https://ploomber.io/" target="blank">
+              <img
+                class="logos"
+                src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/ploomber-removebg-preview_ylm6jf.png"
+                alt="Ploomber"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <div className="partners">
+        <p>MEDIA PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>AUTHENTICATION PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>MENTAL HEALTH PARTNERS</p>
+      </div>
+      <div className="partners">
+        <p>CODING PARTNERS</p>
+      </div> */}
+    </div>
+  );
+}
+export default Sponsors;
